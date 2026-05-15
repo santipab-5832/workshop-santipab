d19e62c0f9b56c0c3a7d42b10ccd1812c721a2f0
Santipab5832 <6710210465@psu.ac.th>
2026-05-15 11:37:12 +0700

    Backend: Edit addOrder API make it automatically recognize userId and didn't want to go copy from database anymore

--- server/controller/order.controller.js
+++ server/controller/order.controller.js
@@ -1,54 +1,60 @@
-var orderSchema = require('../models/order.model')
-var productSchema = require('../models/product.model')
+var orderSchema = require("../models/order.model");
+var productSchema = require("../models/product.model");
 
 exports.getOrders = async function (req, res, next) {
-    try{
-        let order = await orderSchema.find({})
-        res.status(200).json({
-            status: 200,
-            message: 'Success',
-            data: order
-        });
-    } catch(error){
-        res.status(500).send(error)
-    }
-}
+  try {
+    let order = await orderSchema.find({});
+    res.status(200).json({
+      status: 200,
+      message: "Success",
+      data: order,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
 exports.getOrdersByProduct = async function (req, res, next) {
-    try{
-        let { id } = req.params;
+  try {
+    let { id } = req.params;
 
-        let order = await orderSchema.find({ productId: productId });
-        res.status(200).json({
-            status: 200,
-            message: 'Success',
-            data: order
-        });
-    } catch(error){
-        res.status(500).send("Error")
-    }
-}
+    let order = await orderSchema.find({ productId: productId });
+    res.status(200).json({
+      status: 200,
+      message: "Success",
+      data: order,
+    });
+  } catch (error) {
+    res.status(500).send("Error");
+  }
+};
 
 exports.addOrder = async function (req, res, next) {
-    try{
-        let { id } = req.params;
-        let { userId, quantity } = req.body;
-        let product = await productSchema.findById(id);
+  try {
+    let { id } = req.params;
+    let { quantity } = req.body;
+    let userId = req.auth.id;
 
-        if (quantity > product.stock) {
-            return res.status(400).send("ไม่สามารถเพิ่ม Order ได้");
-        }
+    let product = await productSchema.findById(id);
 
-        let order = new orderSchema({ userId, productId: id, quantity });
-        await order.save();
-
-        await productSchema.findByIdAndUpdate(id, { $inc: { stock: -quantity } });
-        res.status(201).json({
-            status: 201,
-            message: 'Added order!',
-            data: order
-        });
-    } catch(error){
-        res.status(500).send(error)
+    if (!product) {
+      return res.status(404).send("Product not found");
     }
-}
+
+    if (quantity > product.stock) {
+      return res.status(400).send("ไม่สามารถเพิ่ม Order ได้");
+    }
+
+    let order = new orderSchema({ userId, productId: id, quantity });
+    await order.save();
+
+    await productSchema.findByIdAndUpdate(id, { $inc: { stock: -quantity } });
+    res.status(201).json({
+      status: 201,
+      message: "Added order!",
+      data: order,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
