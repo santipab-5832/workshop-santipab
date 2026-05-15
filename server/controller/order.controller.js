55d474a1e93376058c2089912e889a14f57e4837
Santipab5832 <6710210465@psu.ac.th>
2026-05-15 16:11:46 +0700

    Backend: Add update and delete order API

--- server/controller/order.controller.js
+++ server/controller/order.controller.js
@@ -58,3 +58,63 @@ exports.addOrder = async function (req, res, next) {
     res.status(500).send(error);
   }
 };
+
+exports.updateOrderStatus = async function (req, res, next) {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const allowedStatus = ["pending", "success", "cancel"];
+
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        status: 400,
+        message: "Invalid status",
+      });
+    }
+
+    const order = await orderSchema.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true },
+    );
+
+    if (!order) {
+      return res.status(404).json({
+        status: 404,
+        message: "Order not found",
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: "Updated order status!",
+      data: order,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.deleteOrder = async function (req, res, next) {
+  try {
+    const { id } = req.params;
+
+    const order = await orderSchema.findByIdAndDelete(id);
+
+    if (!order) {
+      return res.status(404).json({
+        status: 404,
+        message: "Order not found",
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: "Deleted order!",
+      data: order,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -7,5 +7,7 @@ var orderController = require('../controller/order.controller');
 router.get('/', verifyToken, orderController.getOrders)
 router.get('/:productId', verifyToken, orderController.getOrdersByProduct);
 router.post('/:id', verifyToken, orderController.addOrder);
+router.patch('/:id/status', [verifyToken, isAdmin], orderController.updateOrderStatus);
+router.delete('/:id', [verifyToken, isAdmin], orderController.deleteOrder);
 
 module.exports = router;
